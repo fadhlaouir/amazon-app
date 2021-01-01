95938d4950b503822b3f458aaa119948cdb05b9e
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-01-01 22:47:42 +0100

    add admin

---
+++ admin/pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <h2>hello</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'hello'
+    }
+  }
+}
+</script>
+
+<style></style>

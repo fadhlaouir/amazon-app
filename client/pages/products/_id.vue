a579fcb97737c2ed420bf55f790f218d100af5e7
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-01-21 21:48:35 +0100

    Breadcrumbs

---
+++ client/pages/products/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <!-- Breadcrumbs -->
+    <div class="a-spacing-top-medium" />
+    <div class="container-fluid">
+      <div class="wayfinding-breadcrumbs-container">
+        <ul class="a-unordered-list a-horizontal a-size-small">
+          <!-- Category -->
+          <li>
+            <span class="a-list-item">
+              <a class="a-link-normal a-color-tertiary" href="#">Category</a>
+            </span>
+          </li>
+          <li>
+            <span class="a-list-item">></span>
+          </li>
+          <!-- Product Title -->
+          <li>
+            <span class="a-list-item">
+              <a class="a-link-normal a-color-tertiary" href="#">Product Title</a>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </main>
+</template>

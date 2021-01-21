44c7942431e97b5427ed9e769454f3533bd5b671
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-01-21 17:15:55 +0100

    Featured Product

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -6,9 +6,108 @@
         <div class="col-xl-2 col-lg-3 md-4 col-sm-4">
           xx
         </div>
+
         <!-- Main Content -->
         <div class="col-xl-10 xol-lg-9 md-8 col-sm-8">
           <FeaturedProduct />
+
+          <div class="mainResults">
+            <ul class="s-result-list">
+              <li class="s-result-item celwidget">
+                <div class="s-item-container">
+                  <!-- Best seller -->
+                  <div class="a-spacing-micro">
+                    <div class="bestSeller">
+                      <a href="#">Best Seller</a>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div class="row">
+                      <!-- Image  -->
+                      <div class="col-sm-3 text-center">
+                        <a href="#">
+                          <img src="/img/raed.jpg" style="width:150px" class="img-fluid">
+                        </a>
+                      </div>
+
+                      <div class="col-sm-9">
+                        <div class="a-row a-spacing-small">
+                          <!-- Title and Date -->
+                          <a href="#" class="a-link-normal">
+                            <h2 class="a-size-medium">
+                              Title and Date
+                              <span class="a-letter-space" />
+                              <span class="a-letter-space" />
+                              <span class="a-size-small a-color-secondary">Jan 21,2021</span>
+                            </h2>
+                          </a>
+                        </div>
+
+                        <!-- Author's name -->
+                        <div class="a-row a-spacing-small">
+                          <span class="a-size-small a-color-secondary">by</span>
+                          <span class="a-size-small a-color-secondary">
+                            <a class="a-link-normal a-text-normal" href="#">Raed Fadhlaoui</a>
+                          </span>
+                        </div>
+
+                        <!-- Shipment -->
+                        <div class="a-row">
+                          <span class="a-size-small">Ships to Tunisia</span>
+                        </div>
+
+                        <!-- Hardcover -->
+                        <div class="row">
+                          <div class="col-sm-7">
+                            <div class="a-row a-spacing-none">
+                              <a href="#" class="a-link-normal a-text-normal">Hardcover</a>
+                            </div>
+                          </div>
+                        </div>
+
+                        <!-- Price -->
+                        <div class="a-row a-spacing-none">
+                          <a href="#" class="a-link-normal a-text-normal">
+                            <span class="a-offscreen">TND 99</span>
+                            <span class="a-color-base sx-zero-spacing">
+                              <span class="sx-price sx-price-large">
+                                <sup class="sx-price-currency">TND</sup>
+                                <sup class="sx-price-whole">99</sup>
+                                <sup class="sx-price-fractional">00</sup>
+                              </span>
+                            </span>
+                          </a>
+                          <span class="a-letter-space" />
+                          <span class="a-size-base-plus a-color-secondary a-text-strike">TND 28.00</span>
+                        </div>
+
+                        <!-- Audible trial -->
+                        <div class="a-row a-spacing-none">
+                          <span class="a-size-small a-color-secondary">Free with Audible trial</span>
+                        </div>
+                        <hr>
+
+                        <!-- Other Formats -->
+                        <span class="a-size-small a-color-secondary">
+                          Other Formats:
+                          <span class="a-letter-space" />
+                          <a href="#" class="a-size-small a-link-normal a-text-normal">Audio CD</a>
+                        </span>
+
+                        <!-- Rating -->
+                        <div class="col-sm-5">
+                          <div class="a-row a-spacing-mini">
+                            <!-- Star Rating -->
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>

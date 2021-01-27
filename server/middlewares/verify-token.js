e94ec86043e3e9d8b9baf3a7eeda39ec7935ec88
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-01-27 14:45:18 +0100

    test JWT on profile aPI

--- server/middlewares/verify-token.js
+++ server/middlewares/verify-token.js
@@ -2,12 +2,14 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function(req, res, next) {
     let token = req.headers["x-access-token"] || req.headers["authorization"];
-    // it's optional => to delete Bearer from token
-    let checkBearer = "Bearer ";
-    if (token.startsWith(checkBearer)) {
-        token = token.slice(checkBearer.length, token.length);
-    }
+
     if (token) {
+        // it's optional => to delete Bearer from token
+        let checkBearer = "Bearer ";
+        if (token.startsWith(checkBearer)) {
+            token = token.slice(checkBearer.length, token.length);
+        }
+
         jwt.verify(token, process.env.SECRET, (err, decoded) => {
             if (err) {
                 res.json({
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/user");
+const verifyToken = require("../middlewares/verify-token");
 
 const jwt = require("jsonwebtoken");
 
@@ -28,6 +29,24 @@ router.post("/auth/signup", async(req, res) => {
                 message: error.message,
             });
         }
+    }
+});
+
+// Profile Route
+router.get("/auth/user", verifyToken, async(req, res) => {
+    try {
+        let foundUser = await User.findOne({ _id: req.decoded._id });
+        if (foundUser) {
+            res.json({
+                success: true,
+                user: foundUser,
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
     }
 });
 

b0a88a3578691020210941b821bef716b66a94b5
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-02-02 10:11:04 +0100

    Signup page Auth Library

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,35 +1,35 @@
 /* eslint-disable */
-const URL = 'http://localhost:5000'
+const URL = "http://localhost:5000";
 
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'client',
+        title: "client",
         meta: [{
-                charset: 'utf-8'
+                charset: "utf-8"
             },
             {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
+                name: "viewport",
+                content: "width=device-width, initial-scale=1"
             },
             {
-                hid: 'description',
-                name: 'description',
-                content: ''
+                hid: "description",
+                name: "description",
+                content: ""
             }
         ],
         link: [{
-                rel: 'icon',
-                type: 'image/x-icon',
-                href: '/favicon.ico'
+                rel: "icon",
+                type: "image/x-icon",
+                href: "/favicon.ico"
             },
             {
-                rel: 'stylesheet',
-                href: '/css/font-awesome/css/all.css'
+                rel: "stylesheet",
+                href: "/css/font-awesome/css/all.css"
             },
             {
-                rel: 'stylesheet',
-                href: '/css/default.css'
+                rel: "stylesheet",
+                href: "/css/default.css"
             }
         ]
     },
@@ -46,17 +46,18 @@ export default {
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
         // https://go.nuxtjs.dev/eslint
-        '@nuxtjs/eslint-module'
+        "@nuxtjs/eslint-module"
     ],
 
     // Modules: https://go.nuxtjs.dev/config-modules
     modules: [
         // https://go.nuxtjs.dev/bootstrap
-        'bootstrap-vue/nuxt',
+        "bootstrap-vue/nuxt",
         // https://go.nuxtjs.dev/axios
-        '@nuxtjs/axios',
+        "@nuxtjs/axios",
         // https://go.nuxtjs.dev/pwa
-        '@nuxtjs/pwa'
+        "@nuxtjs/pwa",
+        "@nuxtjs/auth-next"
     ],
 
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -65,13 +66,29 @@ export default {
         baseURL: URL
     },
 
+    proxy: {
+        "/api": URL
+    },
+
     // PWA module configuration: https://go.nuxtjs.dev/pwa
     pwa: {
         manifest: {
-            lang: 'en'
+            lang: "en"
         }
     },
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {}
-}
+    build: {},
+    auth: {
+        strategies: {
+            local: {
+                endpoints: {
+                    login: {
+                        propertyName: "token"
+                    },
+                    logout: true
+                }
+            }
+        }
+    }
+};
--- client/pages/signup.vue
+++ client/pages/signup.vue
@@ -21,19 +21,34 @@
                 <!-- Your Name -->
                 <div class="a-row a-spacing-base">
                   <label for="ap_customer_name" class="a-form-label">Your name</label>
-                  <input id="ap_customer_name" type="text" class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info">
+                  <input
+                    id="ap_customer_name"
+                    v-model="name"
+                    type="text"
+                    class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info"
+                  >
                 </div>
 
                 <!-- Email -->
                 <div class="a-row a-spacing-base">
                   <label for="ap_customer_name" class="a-form-label">Email</label>
-                  <input id="ap_customer_name" type="email" class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info">
+                  <input
+                    id="ap_customer_name"
+                    v-model="email"
+                    type="email"
+                    class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info"
+                  >
                 </div>
 
                 <!-- Password -->
                 <div class="a-row a-spacing-base">
                   <label for="ap_customer_name" class="a-form-label">Password</label>
-                  <input id="ap_customer_name" type="password" class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info">
+                  <input
+                    id="ap_customer_name"
+                    v-model="password"
+                    type="password"
+                    class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info"
+                  >
                   <!-- alert => to show error messages -->
                   <div class="a-alert-container">
                     <div class="a-alert-content">
@@ -46,9 +61,7 @@
                 <div class="a-row a-spacing-extra-large mb-4">
                   <span class="a-button-primary">
                     <span class="a-button-inner">
-                      <span class="a-button-text">
-                        Create your Amazon account
-                      </span>
+                      <span class="a-button-text" @click="onSignup">Create your Amazon account</span>
                     </span>
                   </span>
                   <div class="a-row a-spacing-top-medium a-size-small">
@@ -77,6 +90,39 @@
 
 <script>
 export default {
-  layout: 'none'
+  layout: 'none',
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async onSignup () {
+      try {
+        const data = {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        }
+        const response = await this.$axios.$post('/api/auth/signup', data)
+        // eslint-disable-next-line no-console
+        console.log('response', response)
+        if (response.success) {
+          this.$auth.loginWith('local', {
+            data: {
+              email: this.email,
+              password: this.password
+            }
+          })
+          this.$router.push('/')
+        }
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log('error', error)
+      }
+    }
+  }
 }
 </script>
